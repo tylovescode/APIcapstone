327e10b14a39fea5e47000eda8d2b092d60fe4c8
Ty Miller <ty.loves.code@gmail.com>
2017-12-21 20:44:36 -0500

    added ticketmaster api info

--- index.js
+++ index.js
@@ -1,94 +1,93 @@
 //Google Maps
 function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          //enter my location
-          center: {lat: 34.7698, lng: -84.9702},
-          zoom: 13,
-          mapTypeId: 'roadmap'
-        });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    //enter starting location
+    center: {lat: 34.7698, lng: -84.9702},
+    zoom: 13,
+    mapTypeId: 'roadmap'
+    });
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-          console.log("Searched place formatted",places[0].formatted_address);
-          // v3.1.0
-          console.log(places[0]);
-          var service = new google.maps.places.PlacesService(map);
-          var latitude=places[0].geometry.viewport.b.b;
-          var longitude=places[0].geometry.viewport.f.b;
-          console.log(longitude);
-        service.nearbySearch({
-          location: {lat: latitude, lng: longitude},
-          radius: 500,
-        }, callback);
-         function callback(results, status) {
-          console.log(results);
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            console.log(results[i]);
-            // createMarker(results[i]);
-          }
-        }
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    console.log("Searched place formatted",places[0].formatted_address);
+    // v3.1.0
+    console.log(places[0]);
+    var service = new google.maps.places.PlacesService(map);
+    var latitude=places[0].geometry.viewport.b.b;
+    var longitude=places[0].geometry.viewport.f.b;
+    console.log(longitude);
+    service.nearbySearch({
+      location: {lat: latitude, lng: longitude},
+      radius: 500,
+    }, callback);
+    function callback(results, status) {
+    console.log(results);
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        console.log(results[i]);
+        // createMarker(results[i]);
       }
+    }
+  }
+
 useLocation(places[0].formatted_address);
 
+    if (places.length == 0) {
+      return;
+    }
 
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
-          if (places.length == 0) {
-            return;
-          }
-
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
-
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
       }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
 
 function useLocation(address) {
   //Simple Weather  
@@ -98,11 +97,6 @@ function useLocation(address) {
     unit: 'f',
     success: function(weather) {
       console.log(weather)
-      // html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      // html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      // html += '<li class="currently">'+weather.currently+'</li>';
-      // html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-      
       $("#weather").html("<h2>The current temperature in "+weather.city+" is "+weather.temp+"&deg;"+weather.units.temp+".</h2><p>The sun will set at "+weather.sunset+".</p>");
     },
     error: function(error) {
@@ -123,5 +117,24 @@ function useLocation(address) {
 //   console.log(error)
 // }
 //   })
-}
-      
+
+//Ticketmaster
+$.ajax({
+  type:"GET",
+  url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=ZiVpCBhveUAxqrDFzahcvahnPLMJxfFS",
+  async:true,
+  dataType: "json",
+  size: 10,
+  success: function(json) {
+              console.log(json)
+              console.log(json._embedded.events[0])
+              $("#events").html(`${json._embedded.events[0]}`);
+              // Parse the response.
+              // Do other things.
+           },
+  error: function(xhr, status, err) {
+              // This time, we do not end up here!
+           }
+});    
+
+};    
