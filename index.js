2e727c3dfdb276f8798f0d86f6317ef77f545bcd
Ty <31898457+tylovescode@users.noreply.github.com>
2017-12-28 14:17:42 -0500

    Added Ticketmaster location info

--- index.js
+++ index.js
@@ -58,34 +58,34 @@ useLocation(places[0].formatted_address, latitude, longitude, map);
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
-    // places.forEach(function(place) {
-    //   if (!place.geometry) {
-    //     console.log("Returned place contains no geometry");
-    //     return;
-    //   }
-    //   var icon = {
-    //     url: place.icon,
-    //     size: new google.maps.Size(71, 71),
-    //     origin: new google.maps.Point(0, 0),
-    //     anchor: new google.maps.Point(17, 34),
-    //     scaledSize: new google.maps.Size(25, 25)
-    //   };
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-    //   // Create a marker for each place.
-    //   markers.push(new google.maps.Marker({
-    //     map: map,
-    //     icon: icon,
-    //     title: place.name,
-    //     position: place.geometry.location
-    //   }));
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
 
-    //   if (place.geometry.viewport) {
-    //     // Only geocodes have viewport.
-    //     bounds.union(place.geometry.viewport);
-    //   } else {
-    //     bounds.extend(place.geometry.location);
-    //   }
-    // });
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
     map.fitBounds(bounds);
   });
 }
@@ -98,75 +98,68 @@ function useLocation(address, latitude, longitude) {
     unit: 'f',
     success: function(weather) {
       console.log(weather)
-      $("#weather").html("<h2>The current temperature in "+weather.city+" is "+weather.temp+"&deg;"+weather.units.temp+".</h2><p>The sun will set at "+weather.sunset+".</p>");
+      // $("#weather").html("<h2>The current temperature in "+weather.city+" is "+weather.temp+"&deg;"+weather.units.temp+".</h2><p>The sun will set at "+weather.sunset+".</p>");
+      $("#weather").html("<h2>In "+weather.city+", it is currently "+weather.text+" and "+weather.temp+"&deg;"+weather.units.temp+".</h2><p>The sun will set at "+weather.sunset+".</p>");
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-//   $.ajax({
-//     url:"https://api.yelp.com/v3/businesses/search?location=07755&term=pizza",
-//     type:"GET",
-//     dataType:"jsonp",
-//     crossDomain:true,
-//     headers:{
-//       "Authorization":"Bearer Fq-Kom1O4EIrGX-vR5JEfeeqdRnbeONdbMGPB6wg0uxdwSIwYoTKFeQQ-HYf0rjIGfGnqSPeDmBlxdFD32wrvqprPikPLnr8yRI-AzbttFd0Gz_lmdghqpALMx84WnYx"
-//     },
-//     success:function(result) {
-//   console.log(result)
-// },error:function(error){
-//   console.log(error)
-// }
-//   })
 
 //Ticketmaster
 $.ajax({
   type:"GET",
-  url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=ZiVpCBhveUAxqrDFzahcvahnPLMJxfFS&latlong="+longitude+","+latitude,
+  url:"https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=ZiVpCBhveUAxqrDFzahcvahnPLMJxfFS&latlong="+longitude+","+latitude,
   async:true,
   dataType: "json",
   size: 10,
   success: function(json) {
+              //if another location is searched for, clear event results from old location
+              $('#events').empty();  
               console.log(json)
               console.log(json._embedded.events[0])
+              console.log(json._embedded.events[0]._embedded.venues[0].location)
               $.each(json._embedded.events, function(index, value) {
+              $('#going-on').html("<h3>Here's what is going on near "+address+"</h3>")
               $('#events').append("<h4>"+value.name+"</h4>")
               })
-var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+            },
+
+
+// var locations = [
+//       ['Bondi Beach', -33.890542, 151.274856, 4],
+//       ['Coogee Beach', -33.923036, 151.259052, 5],
+//       ['Cronulla Beach', -34.028249, 151.157507, 3],
+//       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+//       ['Maroubra Beach', -33.950198, 151.259302, 1]
+// //     ];
 
     
 
-    var infowindow = new google.maps.InfoWindow();
+//     var infowindow = new google.maps.InfoWindow();
 
-    var marker, i;
+//     var marker, i;
 
-    for (i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
+//     for (i = 0; i < locations.length; i++) {  
+//       marker = new google.maps.Marker({
+//         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+//         map: map
+//       });
 
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
+//       google.maps.event.addListener(marker, 'click', (function(marker, i) {
+//         return function() {
+//           infowindow.setContent(locations[i][0]);
+//           infowindow.open(map, marker);
+//         }
+//       })(marker, i));
+//     }
   
-              //$("#events").html(`${json._embedded.events[0]}`);
-              // Parse the response.
-              // Do other things.
-           },
+//               //$("#events").html(`${json._embedded.events[0]}`);
+//               // Parse the response.
+//               // Do other things.
+//            },
   error: function(xhr, status, err) {
-              // This time, we do not end up here!
            }
 });    
-
-};    
+};
+    
